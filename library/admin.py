f70721c8510db3bcbcd38e8443b3db478412649a
Selva73582 <nobiselva73582@gmail.com>
2024-01-30 20:15:00 +0530

    first commit

---
+++ library/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+from . import models
+
+
+admin.site.register(models.UserProfile)
+admin.site.register(models.Experience)
+admin.site.register(models.Posting)
+admin.site.register(models.Certifications)
